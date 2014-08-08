adad6ccd4bf31bc9a966e0ec2a0ef0db7e806d44
spavlick <spavlick@mit.edu>
2014-08-08 11:04:35 -0400

    Beginning to build the GUI

--- gui.py
+++ gui.py
@@ -1,3 +1,11 @@
-import Tkinter
-top=Tkinter.Tk()
-top.mainloop()
+from Tkinter import *
+from ttk import *
+
+root=Tk()
+root.title('Planar Magnetics Analyzing System (PMAS)')
+
+#create frame for the GUI
+mainframe=Frame(root)
+
+
+root.mainloop() #execute at end of code
