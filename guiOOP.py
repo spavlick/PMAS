e9a3aa8ce6e5bf99f94c5c3b943bb3237a1a37b1
spavlick <spavlick@mit.edu>
2014-08-20 13:52:09 -0400

    Code to check geometry

--- guiOOP.py
+++ guiOOP.py
@@ -1,7 +1,9 @@
 from Tkinter import *
 from ttk import *
 import tkFileDialog
+import tkMessageBox
 import math
+import literal_eval
 
 SIGMA=5.8e7
 MU0=4*math.pi*1e-7
@@ -16,7 +18,7 @@ class GUI(Frame):
     options['defaultextension']='.txt'
     options['filetypes']=[('all files','.*'),('text files','.txt')]
     options['initialdir']='C:\\'
-    options['initialfile']='geometry.txt'
+    options['initialfile']='file.txt'
     options['parent']=root
 
     self.dir_opt=options={}
@@ -79,27 +81,40 @@ class GUI(Frame):
     self.entries['c']=self.centry
 
     self.geofilename='geometry.txt'
+    self.netlistfilename='netlist.txt'
+
+    #variables for getImpe function
+    self.Ra=None
+    self.La=None
+    self.Rb=None
+    self.Lb=None
+    self.Ls=None
+    self.Lfb=None
+    self.Lft=None
 
     #call functions to display interface
     self.printlabels()
     self.entrygrid()
     self.createbuttons()
 
-  def askopenfilename(self):
+  def askopengeofilename(self):
     self.geofilename=tkFileDialog.askopenfilename(**self.file_opt)
 
-  def asksaveasfilename(self):
+  def asksaveasgeofilename(self):
     self.geofilename=tkFileDialog.asksaveasfilename(**self.file_opt)
+  
+  def asksaveasnetlistfilename(self):
+    self.netlistfilename=tkFileDialog.asksaveasfilename(**self.file_opt)
 
   def savegeom(self):
-    self.asksaveasfilename()
+    self.asksaveasgeofilename()
     f=open(self.geofilename,'w')
     for key in self.entries.keys():
       f.write(key + ' = ' + self.entries[key].get() + '\n')
     f.close()
 
   def loadgeom(self):
-    self.askopenfilename()
+    self.askopengeofilename()
     f=open(self.geofilename,'r')
     for line in f:
       self.entries[line.split()[0]].insert(0,line.split()[2])
@@ -109,7 +124,18 @@ class GUI(Frame):
     for key in self.entries.keys():
       self.entries[key].delete(0,END)
     
-  
+  def checkgeom(self):
+    nwinding=int(self.nwinding.get()) 
+    nlayer=int(self.nlayer.get())   
+
+    if nwinding!= max(literal_eval(self.lindex.get())):
+      tkMessageBox.showerror('NumofWinding and WindingIndex do not match')
+    if nwinding!= len(literal_eval(self.wstyle.get())):
+      tkMessageBox.showerror('NumofWinding and WindingStyle do not match')
+    if nlayer!=len(literal_eval(self.h.get())):
+      tkMessageBox.showerror('NumofLayer mismatch with h, please revise #layer or #h')
+    if nlayer!=len(literal_eval(self.s.get())):
+      tkMessageBox.showerror('NumofLayer mismatch with h\s, please revise #layer or #s')
     
 
   def printlabels(self):
@@ -167,47 +193,156 @@ class GUI(Frame):
     Button(self,text='Load Geometry',command=self.loadgeom).grid(row=0,column=0)
     Button(self,text='Save Geometry',command=self.savegeom).grid(row=0,column=1)
     Button(self,text='Reset Geometry',command=self.resetgeom).grid(row=0,column=2)
-    Button(self,text='Check Geometry Status').grid(row=16,columnspan=3)
-    Button(self,text='Generate Netlist').grid(row=18,columnspan=3)
+    Button(self,text='Check Geometry Status',command=self.checkgeom).grid(row=16,columnspan=3)
+    Button(self,text='Generate Netlist',command=self.generate_netlist).grid(row=18,columnspan=3)
 
   def getImpe(self):
+    le=float(self.le.get())
+    nc=float(self.nc.get())
+    h=literal_eval(self.h.get())
+    NumofLayer=int(self.nlayer.get())
+    mur=float(self.mur.get())
+    s=literal_eval(self.s.get())
+    w=literal_eval(self.w.get())
+    g=float(self.g.get())
+    f=float(self.f.get())
+    c=float(self.c.get())
+    Ae=float(self.Ae.get())
 
     d=le*nc #effective length of the winding
-
-    #check length of input matrices
-    if NumofLayer!=len(h):
-      print 'NumofLayer mismatch with h, please revise #layer or #h'
-    if NumofLayer!=len(s):
-      print 'NumofLayer mismatch with h\s, please revise #layer or #s'
 
     Xa=[]
     Xb=[]
     Xs=[]
     for i1 in range(NumofLayer):
-      delta=(2/(f*2*math.pi)/mu0/sigma)**0.5
+      delta=(2/(f*2*math.pi)/MU0/SIGMA)**0.5
       Psi=complex(1/delta,1/delta)
-      Z=Psi/sigma
+      Z=Psi/SIGMA
       A=cmath.exp(-Psi*h[i1])
       Za=Z*(1-A)/(1+A)
       Zb=Z*2*A/(1-A**2)
       Xa.append(d/w[i1]*Za)
       Xb.append(d/w[i1]*Zb)
-      Xs.append(complex(0,1)*(f*2*math.pi)*mu0*s[i1]*d/w[i1])
+      Xs.append(complex(0,1)*(f*2*math.pi)*MU0*s[i1]*d/w[i1])
 
     #impedance for the ferrite core
-    Xfb=complex(0,1)*(f*2*math.pi)*mu0*Ae/(g+Ae*w[i1]/(mur*c*d))
-    Xft=complex(0,1)*(f*2*math.pi)*mu0*mur*c*d/w[i1]
+    Xfb=complex(0,1)*(f*2*math.pi)*MU0*Ae/(g+Ae*w[i1]/(mur*c*d))
+    Xft=complex(0,1)*(f*2*math.pi)*MU0*mur*c*d/w[i1]
 
     #calculate output
-    Ra=numpy.array([e.real for e in Xa])
-    La=numpy.array([e.imag for e in Xa])/(f*2*math.pi)
-    Rb=numpy.array([e.real for e in Xb])
-    Lb=numpy.array([e.imag for e in Xb])/(f*2*math.pi)
-    Ls=numpy.array([e.imag for e in Xs])/(f*2*math.pi)
-    Lfb=Xfb.imag/(f*2*math.pi)
-    Lft=Xft.imag/(f*2*math.pi)
+    self.Ra=numpy.array([e.real for e in Xa])
+    self.La=numpy.array([e.imag for e in Xa])/(f*2*math.pi)
+    self.Rb=numpy.array([e.real for e in Xb])
+    self.Lb=numpy.array([e.imag for e in Xb])/(f*2*math.pi)
+    self.Ls=numpy.array([e.imag for e in Xs])/(f*2*math.pi)
+    self.Lfb=Xfb.imag/(f*2*math.pi)
+    self.Lft=Xft.imag/(f*2*math.pi)
 
-    return [Ra,La,Rb,Lb,Ls,Lfb,Lft]
+  def generate_netlist(self):
+
+    self.asksaveasnetlistfilename()
+
+    mur=float(self.mur.get())
+    NumofLayer=int(self.nlayer.get())
+    NumofWinding=int(self.nwinding.get())
+    h=literal_eval(self.h.get())
+    s=literal_eval(self.s.get())
+    w=literal_eval(self.w.get())
+    m=float(self.m.get())
+    g=float(self.g.get())
+    Ae=float(self.Ae.get())
+    le=float(self.le.get())
+    nc=int(self.nc.get())
+    c=float(self.c.get())
+
+    self.getImpe()
+
+    #Repeat and summarizing the input information
+    Serieslayers={}
+    f=open(self.netlistfilename,'w')
+    f.write('*Summary of the Transformer Structure in the I/O Ports')
+    f.write('\n*There are {} Windings in total'.format(NumofWinding))
+    for index_winding in range(NumofWinding):
+      #Parallel Connected
+      if WindingStyle[index_winding]==1:
+        f.write('\n*Winding {0} is Parallel Connected with Winding Port PortP{0} and PortN{0}'.format(index_winding))
+        for index_layer in range(NumofLayer):
+          if WindingIndex[index_layer]==index_winding:
+            f.write('\n*Include Layer {}, thickness {}, width {}, turns {}, spacing {}'.format(index_layer, h[index_layer], w[index_layer], m[index_layer], s[index_layer]))
+            break
+      if WindingStyle[index_winding]==0:
+        f.write('\n*Winding {0} is Series Connected with Winding Port Port{0} and PortN{0}'.format(index_winding))
+        numSeriesLayers=1
+        for index_layer in range(NumofLayer):
+          if WindingIndex[index_layer]==index_winding:
+            f.write('\n*Include Layer {}, thickness {}, width {}, turns {}, spacing {}'.format(index_layer,h[index_layer],w[index_layer],m[index_layer],s[index_layer]))
+            Serieslayers[numSeriesLayers]=index_layer+1 
+            numSeriesLayers+=1
+
+    #Generate the SPICE netlist
+    for index in range(NumofLayer):
+      ra=Ra[index]
+      la=La[index]
+      rb=Rb[index]
+      lb=Lb[index]
+      ls=Ls[index]
+      mx=m[index]
+
+      f.write('\n*NetList for Layer {}'.format(index+1))
+      f.write('\nLe{0} N{0} P{0} {1}'.format(index,mx**2))
+      f.write('\nLi{0} G Md{0} {1}'.format(index,1))
+      f.write('\nLg{0} Mg{0} Md{0} {1:14.2f}p'.format(index,lb*1e12))
+      f.write('\nRg{0} Mc{0} Mg{0} {1:14.2f}m'.format(index,rb*1e3))
+      f.write('\nRt{0} Mc{0} Mt{0} {1:14.2f}u'.format(index,ra*1e6))
+      f.write('\nRb{0} Mb{0} Mc{0} {1:14.2f}u'.format(index,ra*1e6))
+      f.write('\nLt{0} T{0} Mt{0} {1:14.2f}p'.format(index,la*1e12))
+      f.write('\nLb{0} Mb{0} B{0} {1:14.2f}p'.format(index,la*1e12))
+      f.write('\nLs{0} B{0} T{1} {2:14.2f}p'.format(index,index+1,ls*1e12))
+      f.write('\nK{0} Le{0} Li{0} 1'.format(index))
+
+    #Print the ferrite cores
+    f.write('\n*******************************')
+    f.write('\n*NetList for Top and Bottom Ferrites')
+    f.write('\nLft T1 G {:14.2f}u'.format(Lft*1e6))
+    f.write('\nLfb T{} G {:14.2f}u'.format(NumofLayer+1,Lfb*1e6))
+
+    #Print the external connections
+    f.write('\n**************************')
+    f.write('\n*Winding Connections')
+    f.write('\n*Using ZERO ohm resistors')
+
+    #Create External Winding Ports
+    for index_winding in range(NumofWinding):
+      #Parallel Connected
+      if WindingStyle[index_winding]==1:
+        f.write('\n*Winding {} is Parallel Connected'.format(index_winding+1))
+        for index_layer in range(NumofLayer):
+          if WindingIndex[index_layer]==index_winding+1:
+            f.write('\n*Include layer {}'.format(index_layer+1))
+            f.write('\nRexP{0} PortP{1} P{0} 1n'.format(index_layer+1,index_winding+1))
+            f.write('\nRexN{0} PortN{1} N{0} 1n'.format(index_layer+1,index_winding+1))
+
+
+      #Series Connected
+      if WindingStyle[index_winding]==0:
+        f.write('\n*Winding {} is Series Connected'.format(index_winding+1))
+        
+        #identify which layers it contains
+        numSeriesLayers=1
+        for index_layer in range(NumofLayer):
+          if WindingIndex[index_layer]==index_winding+1:
+            f.write('\n*Include layer {}'.format(index_layer+1))
+            Serieslayers[numSeriesLayers]=index_layer+1
+            numSeriesLayers+=1
+        f.write('\nRexP{0} PortP{1} P{0} 1n'.format(Serieslayers[1],index_winding+1))
+        f.write('\nRexN{0} PortN{1} N{0} 1n'.format(Serieslayers[numSeriesLayers-1],index_winding+1))
+        for index_SeriesLayers in range(numSeriesLayers-2):
+          f.write('\nRexM{0} N{0} P{1} 1n'.format(Serieslayers[index_SeriesLayers+1],Serieslayers[index_SeriesLayers+2]))
+
+    #netlist finalized
+    f.write('\n***************************')
+    f.write('\n*This is the END of the Netlist')
+    f.close()
     
 
 
